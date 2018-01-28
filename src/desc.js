3aeabd87885d0972436f0353b9aedc9922f738b8
Alexander Lee <alex44sport@gmail.com>
2018-01-28 03:48:45 +0000

    Important fixes to generate a production build.

--- src/desc.js
+++ src/desc.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MyLinks from './mylinks'
 
-const desc = () => (
-  <div id="desc">
-    <div id="desc_text">
-      <h1 id="desc_header">
-        Hey, I'm Alex!
-      </h1>
-      <div>
-        I am a Junior @ UIUC studying Computer Engineering with a minor in Statistics. I will be at Jump Trading this summer writing highly performant C++ code for their algorithmic trading systems. I was previously at JHU-APL, CME Group, CapitalOne, and Leidos. Check out my social media and portfolio!
+class desc extends Component {
+  render() {
+    return (
+      <div id="desc">
+        <div id="desc_text">
+          <h1 id="desc_header">Hey, I'm Alex!</h1>
+          <div>
+            I am a Junior @ UIUC studying Computer Engineering with a minor in Statistics. I will be at Jump Trading this summer writing highly performant C++ code for their algorithmic trading systems. I was previously at JHU-APL, CME Group, CapitalOne, and Leidos. Check out my social media and portfolio!
+          </div>
+        </div>
+        <div id="links">
+          <MyLinks/>
+        </div>
       </div>
-    </div>
-    <div id="links">
-      <MyLinks/>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default desc;
--- src/mylinks.js
+++ src/mylinks.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import IconButton from 'material-ui/IconButton';
-import { Linkedin, GithubCircle, FileDocument, Twitter } from 'mdi-material-ui'
-import {blue500, greenA700, white} from 'material-ui/styles/colors';
+import Linkedin from 'mdi-material-ui/Linkedin'
+import GithubCircle from 'mdi-material-ui/GithubCircle'
+import FileDocument from 'mdi-material-ui/FileDocument'
+import Twitter from 'mdi-material-ui/Twitter'
+import { blue500, greenA700, white } from 'material-ui/styles/colors';
 
-const MyLinks = () => (
-  <div>
-    <IconButton href="https://www.linkedin.com/in/mdalexanderlee/" target="_blank"><Linkedin hoverColor={blue500}/></IconButton>
-    <IconButton href="https://github.com/mdalexanderlee" target="_blank"><GithubCircle hoverColor={white}/></IconButton>
-    <IconButton href={require("./Alexander_Lee_CS.pdf")} target="_blank"><FileDocument hoverColor={greenA700}/></IconButton>
-    <IconButton href="https://twitter.com/thisisalexlee" target="_blank"><Twitter hoverColor={blue500}/></IconButton>
-  </div>
-);
+class MyLinks extends Component {
+  render() {
+  	return (
+	  <div>
+	    <IconButton href="https://www.linkedin.com/in/mdalexanderlee/" target="_blank"><Linkedin hoverColor={blue500}/></IconButton>
+	    <IconButton href="https://github.com/mdalexanderlee" target="_blank"><GithubCircle hoverColor={white}/></IconButton>
+	    <IconButton href={require("./Alexander_Lee_CS.pdf")} target="_blank"><FileDocument hoverColor={greenA700}/></IconButton>
+	    <IconButton href="https://twitter.com/thisisalexlee" target="_blank"><Twitter hoverColor={blue500}/></IconButton>
+	  </div>
+    );
+  }
+}
 
 export default MyLinks;
