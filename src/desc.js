e8554b69317000ae54a5e2b33feea82640a67e07
Alexander Lee <alex44sport@gmail.com>
2018-01-28 17:22:45 +0000

    Added typing animation.

--- src/desc.js
+++ src/desc.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import MyLinks from './mylinks'
+import Typist from 'react-typist';
+import '../node_modules/react-typist/dist/Typist.css';
+
+var style = {
+  show: true,
+  blink: true,
+  element: '|',
+  hideWhenDone: true,
+  hideWhenDoneDelay: 3000,
+}
 
 class desc extends Component {
   render() {
     return (
       <div id="desc">
         <div id="desc_text">
-          <h1 id="desc_header">Hey, I'm Alex!</h1>
-          <div>
-            I am a Junior @ UIUC studying Computer Engineering with a minor in Statistics. I will be at Jump Trading this summer writing highly performant C++ code for their algorithmic trading systems. I was previously at JHU-APL, CME Group, CapitalOne, and Leidos. Check out my social media and portfolio!
-          </div>
+          <Typist cursor={style} avgTypingDelay={35}>
+            <h1 id="desc_header">Hey, I'm Alex!</h1>
+            <br/>
+            <Typist.Delay ms={2500} />
+            <span>
+              I am a Junior @ UIUC studying Computer Engineering with a minor in Statistics. <Typist.Delay ms={1000} />I will be at Jump Trading this summer writing highly performant C++ code for their algorithmic trading systems. <Typist.Delay ms={1000} />I was previously at JHU-APL, CME Group, CapitalOne, and Leidos. <Typist.Delay ms={1000} />Check out my social media and portfolio!
+            </span>
+          </Typist>
         </div>
         <div id="links">
           <MyLinks/>
