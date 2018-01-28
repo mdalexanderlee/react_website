7852b53640c1d32c7cb23068a6731ecb0bc921ea
Alexander Lee <alex44sport@gmail.com>
2018-01-28 18:00:20 +0000

    Fade in links.

--- src/desc.js
+++ src/desc.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import MyLinks from './mylinks'
 import Typist from 'react-typist';
 import '../node_modules/react-typist/dist/Typist.css';
+import FadeIn from 'react-fade-in';
 
 var style = {
   show: true,
@@ -12,11 +13,24 @@ var style = {
 }
 
 class desc extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show_links: false,
+    };
+    this.onTypingIsDone = this.onTypingIsDone.bind(this)
+  }
+
+  onTypingIsDone(){
+    this.setState({
+      show_links: true,
+    });
+  }
   render() {
     return (
       <div id="desc">
         <div id="desc_text">
-          <Typist cursor={style} avgTypingDelay={35}>
+          <Typist cursor={style} avgTypingDelay={35} onTypingDone={this.onTypingIsDone}>
             <h1 id="desc_header">Hey, I'm Alex!</h1>
             <br/>
             <Typist.Delay ms={2500} />
@@ -27,13 +41,12 @@ class desc extends Component {
               <Typist.Delay ms={1000} />
               Previously, I've interned at JHU-APL, CME Group, CapitalOne, and Leidos.&nbsp;
               <Typist.Delay ms={1000} />
-              Check out my social media and portfolio! 
+              Check out my social media and portfolio!
+              <Typist.Delay ms={1000} /> 
             </span>
           </Typist>
         </div>
-        <div id="links">
-          <MyLinks/>
-        </div>
+        {this.state.show_links ? <FadeIn><div id="links"><MyLinks/></div></FadeIn> : <p>&nbsp;</p>}
       </div>
     );
   }
